908b062940de72b9677e51eb9c3a6fd49381b059
26Kartikay <121488635+26Kartikay@users.noreply.github.com>
2025-06-30 18:23:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,4 +117,5 @@ def download():
     return send_file(pdf_name, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
